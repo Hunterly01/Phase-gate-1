88ae5708893d00c7cfd6ccd800f05190df81a63e
Hunterly01 <mhunterly@gmail.com>
2025-08-19 17:32:34 +0100

    m

--- java/Day2/TaskThree.java
+++ java/Day2/TaskThree.java
@@ -7,13 +7,13 @@ let count = array.length();
 if(count == 0){
 	return "no one like this";
 } else if(count === 1){
-	return array[0], "like this";
+	return array[0] + "," + "like this";
 } else if(count === 2){
-	return array[0], array[1], "like this`;
+	return array[0] + "," +array[1] + "," "like this`;
 }else if(count === 3){
-	return {array[0],${array[1]} and array[2] "like this`;
+	return {array[0] + "," + {array[1]} + "," + "and " + array[2] + ", " + "like this`;
 }else if(count > 3){
-	return array[0],array[1]and (array.length - 2) others "like this`;
+	return array[0] + "," + array[1] + "," + "and" + (array.length - 2) + "others like this`;
 }
 }
 
