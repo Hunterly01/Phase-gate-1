6b9511491acecfb4b3dc55a818ce9f64777a4cfd
Hunterly01 <mhunterly@gmail.com>
2025-08-21 19:11:02 +0100

    done

--- python/Day4/grocery_manager_app.py
+++ python/Day4/grocery_manager_app.py
@@ -1,50 +1,54 @@
 new_array = [];
 def add_my_item(add_item):
 	new_array.append(add_item);
-	return new_array;
+	return f"'{add_item}' has been added to your grocery list.";
 
 def remove_my_item(remove_item):
 	new_array.remove(remove_item);
-	return new_array;
+	return f"'{remove_item}' has been removed from your grocery list.";
 
 def to_show_item():
 	if not new_array:
-		return "List is empty";
-	for count in new_array:
-		new_array[count];
+		return "your grocery List is empty";
+	else:
+		for item in new_array:
+			return new_array;
 
 
-
-while(True):
-	name = """ 
-			WELCOME TO YOUR GROCERY STORE
+def main():
+	while(True):
+		name = """ 
+				WELCOME TO YOUR GROCERY STORE
 		   
 		  
-		  	1.Add item
-		  	2.Remove item
-		  	3.Show Available item
-		  	0.exit
-			""";
-	print(name);
-	option = int(input("Enter a option:"))
-	match(option):
-		case 1:
-			userInput = input("1.Add item:"); 
-			add_my_item(userInput);
-			break;
+		  		1.Add item
+		  		2.Remove item
+		  		3.Show Available item
+		  		0.exit
+				""";
+		print(name);
+		option = int(input("Enter a option:"))
+		match(option):
+			case 1:
+				userInput = input("1.Add item:"); 
+				print(add_my_item(userInput));
+			
 
-		case 2:
-			userInput = input("2.Remove item:"); 
-			remove_my_item(userInput);
-			break;
+			case 2:
+				userInput = input("2.Remove item:"); 
+				print(remove_my_item(userInput));
+			
 
-		case 3:
-			to_show_item();
-			break;
-		case 0:
-			while False:
-				print("bye bye");
-			break;
+			case 3:
+				print(to_show_item());
+			
+			case 0:
+				while False:
+					print("bye bye");
+				break;
+			case _:
+				print(" Invalid option. Please try");
 			 
 			
-		
+if __name__ == "__main__":
+    main()
